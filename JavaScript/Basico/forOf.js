bdadfe0d9eb5ec89ddde3d67120b5b501b993d29
MauricioL88 <lisboa.sse@gmail.com>
2024-10-27 11:59:04 -0300

    trabalhando com for of

---
+++ JavaScript/Basico/forOf.js
@@ -0,0 +1,11 @@
+const nome = 'Mauricio Lisboa'
+
+// for of exibe os valores do elemento
+for(let valor of nome) {
+    console.log(valor);    
+}
+
+/**
+    For of não funciona com objetos pois acusa
+    do elemento não ser iterável
+ */
